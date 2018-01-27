f49eb7e0f0595b4f5f42d2d403e83ae0b00db339
adsoftsito <adsoft@live.com.mx>
2018-01-27 17:34:39 -0600

    initial commit

---
+++ ch1/helloworld.py
@@ -0,0 +1,11 @@
+myvar = 3
+myvar += 2
+print (myvar)
+
+myvar -= 1
+print (myvar)
+
+mystring = "Hello"
+mystring += " world"
+print (mystring)
+
---
+++ ch1/pythonsuma.py
@@ -0,0 +1,9 @@
+x=1
+y=x+9
+print(y)
+
+import tensorflow as tf
+x=tf.constant(1, name='x')
+y=tf.Variable(x+9, name='y')
+print (y)
+
